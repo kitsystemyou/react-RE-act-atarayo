7ea8d149f236b331dd64c9f8ba118f87507566c9
kitsystemyou <kitsystemyou@gmail.com>
2022-12-18 02:03:29 +0900

    init react-router

---
+++ src/components/Character.js
@@ -0,0 +1,17 @@
+import './Character.css';
+
+export default function MyAccount() {
+    return (
+      <div>
+        <div className="logo-space">
+          <img src="https://pbs.twimg.com/profile_images/1302068627458330625/35ruJoSf_400x400.jpg"
+          className="logo" alt="logo" />
+        </div>
+        <div className="Character">
+        <main style={{ padding: '1rem 0' }}>
+          <h2>Character</h2>
+        </main>
+        </div>
+      </div>
+    );
+  }
